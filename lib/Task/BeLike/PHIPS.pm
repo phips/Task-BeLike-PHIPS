fe6761afe39ac818ba52fb928a16c9423974fb33
Mark Phillips <mark@probably.co.uk>
2013-06-05 23:00:10 +0200

    Initial version

---
+++ lib/Task/BeLike/PHIPS.pm
@@ -0,0 +1,109 @@
+package Task::BeLike::PHIPS;
+
+use strict;
+use warnings;
+use 5.010;
+use vars qw{$VERSION};
+
+BEGIN {
+    $VERSION = '0.0.1';
+}
+ 
+1;
+
+__END__
+
+=head1 NAME
+
+Task::BeLike::PHIPS - My favourite and frequently used modules
+
+
+=head1 VERSION
+
+This document describes Task::BeLike::PHIPS version 0.0.1
+
+
+=pkgroup Perl development tools
+
+=pkg Modern::Perl
+
+=pkg Devel::NYTProf
+
+=pkg DDP
+
+=pkg Module::Install
+
+=pkg Perl::Tidy
+
+=pkg Perl::Critic
+
+=pkg Perl6::Export::Attrs
+
+=pkg ExtUtils::MakeMaker
+
+=pkg Module::Install::GithubMeta;
+
+
+=pkgroup Web
+
+=pkg IO::Socket::SSL
+
+=pkg Mojolicious
+
+=pkg Plack
+
+=pkg WWW::Mechanize
+
+=pkg WWW::RobotRules
+
+=pkg WWW::UserAgent::Random
+
+=pkg LWP
+
+
+=pkgroup Testing
+
+=pkg Test::Perl::Critic
+
+=pkg Test::Pod::Coverage
+
+
+=pkgroup Shell tools
+
+=pkg Term::ReadKey
+
+=pkg Term::ReadLine::Gnu
+
+=pkg Daemon::Control
+
+
+=pkgroup Data manipulation
+
+=pkg YAML::XS
+
+
+=pkgroup CPAN
+
+=pkg App::cpanoutdated
+
+=pkg Pod::Cpandoc
+
+
+
+
+=head1 AUTHOR
+
+Mark Phillips  C<< <mark@probably.co.uk> >>
+
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2013, Mark Phillips C<< <phips@cpan.org> >>. All rights reserved.
+
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
+
+=cut
+
+# vim: set ts=4 sw=4 et:
+
