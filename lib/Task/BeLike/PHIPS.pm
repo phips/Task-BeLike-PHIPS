9b27c912ea101c768a7736514c2693e3c19cf2e0
Mark Phillips <mark@probably.co.uk>
2013-11-03 21:59:28 +0000

    EV, AnyEvent

--- lib/Task/BeLike/PHIPS.pm
+++ lib/Task/BeLike/PHIPS.pm
@@ -6,7 +6,7 @@ use 5.010;
 use vars qw{$VERSION};
 
 BEGIN {
-    $VERSION = '0.0.9';
+    $VERSION = '0.1.0';
 }
  
 1;
@@ -20,7 +20,7 @@ Task::BeLike::PHIPS - My favourite and frequently used modules
 
 =head1 VERSION
 
-version 0.0.9
+version 0.1.0
 
 =head1 TASK CONTENTS
 
@@ -66,6 +66,13 @@ version 0.0.9
 =head3 L<LWP::Protocol::https>
 
 =head3 L<WebService::Amazon::Route53>
+
+
+=head2 Events
+
+=head3 L<EV>
+
+=head3 L<AnyEvent>
 
 
 =head2 Testing
